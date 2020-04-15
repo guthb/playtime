9ccb30b459bcac0bbcda619ba19806c3d6155c4e
Bradley Guthrie <guppy96@gmail.com>
2020-04-15 17:42:17 -0500

    new navbar tweaks

--- 7/ng-fundamentals/src/app/nav/navbar.component.ts
+++ 7/ng-fundamentals/src/app/nav/navbar.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styles: [`
+    .nav.navbar-nav {fonr-size: 15px}
+    #searchForm {margin-right: 100px;}
+    @media (max-width: 1200px) {#searchForm {display: none}}
+  `]
 })
 
 export class NavBarComponent {
