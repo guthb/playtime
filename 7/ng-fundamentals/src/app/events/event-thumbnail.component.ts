1f844a7e118f9f96581fd8b2809779c0e22af2de
Bradley Guthrie <guppy96@gmail.com>
2020-04-18 07:38:12 -0500

    adding in data around time of event

--- 7/ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ 7/ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -6,17 +6,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
       <div class="well hoverwell thumbnail">
         <h2> {{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <div [ngSwitch]="event?.time">
+          Time: {{event?.time}}
+          <span *ngSwitchCase="'8:00 am'">(Early Time)</span>
+          <span *ngSwitchCase="'10:00 am'">(Late Time)</span>
+          <span *ngSwitchDefault >(Normal Time)</span>
+        </div>
         <div>Price: \${{ event?.price }}</div>
-        <!-- <div *ngIf="event?.location"> -->
-        <div [hidden]="!event?.location">
+        <div *ngIf="event?.location">
+        <!-- <div [hidden]="!event?.location"> -->
           <span>Location: {{event?.location?.address}}</span>
           <!-- <span>&nbsp;</span> -->
           <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
         <!-- <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button> -->
-        <!-- <div *ngIf="event?.onlineUrl"> -->
-        <div [hidden]="!event?.onlineUrl">
+        <div *ngIf="event?.onlineUrl">
+        <!-- <div [hidden]="!event?.onlineUrl"> -->
           Online URL: {{event?.onlineUrl}}
         </div>
       </div>
