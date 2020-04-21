2f26c605b5fbadd498b78b5ff93c9cfd55052232
Bradley Guthrie <guppy96@gmail.com>
2020-04-21 15:00:02 -0500

    added router

--- 7/ng-fundamentals/src/app/app.module.ts
+++ 7/ng-fundamentals/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { EventsAppComponent } from './events-app.component';
-
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { EventDetailsComponent } from './events/event-details/event-details.comp
     EventDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
