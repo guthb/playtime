98032491429c8c1bc5989b73e924539d27cddb31
Bradley Guthrie <guppy96@gmail.com>
2020-04-21 14:20:36 -0500

    adding the styling

--- 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './event-details.component.html',
-  styleUrls: ['./event-details.component.css']
+  styleUrls: [`
+    .container {padding-left: 20px; padding-right: 20px;}
+    .event-image: {height: 100px;}
+  `]
 })
 export class EventDetailsComponent implements OnInit {
 
