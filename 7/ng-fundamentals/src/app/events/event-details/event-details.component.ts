1d018d1dc8ee606d3dbff56eb31ee1fac91929aa
Bradley Guthrie <guppy96@gmail.com>
2020-04-21 14:16:41 -0500

    creating new comp for routing

---
+++ 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './event-details.component.html',
+  styleUrls: ['./event-details.component.css']
+})
+export class EventDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
