e635fffd28fd11ec49f7df1e04305c28a1246c07
Bradley Guthrie <guppy96@gmail.com>
2020-04-22 07:09:53 -0500

    addedd activated route

--- 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -10,7 +11,8 @@ import { EventService } from '../shared/event.service';
 })
 export class EventDetailsComponent implements OnInit {
   event: any
-  constructor(private eventService: EventService) { }
+
+  constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.event = this.eventService.getEvent(1)
