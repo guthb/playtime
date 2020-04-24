4c6946ab371e4700992cd5cfc9bf8d91c134d8b0
Bradley Guthrie <guppy96@gmail.com>
2020-04-24 12:10:06 -0500

    missed that the value should be int for method

--- 7/ng-fundamentals/src/app/events/event-details/event-route-activator.service.ts
+++ 7/ng-fundamentals/src/app/events/event-details/event-route-activator.service.ts
@@ -8,7 +8,7 @@ export class EventRouterActivator implements CanActivate {
 
   }
   canActivate(route: ActivatedRouteSnapshot) {
-    const eventExists = !!this.eventService.getEvent(route.params['id'])
+    const eventExists = !!this.eventService.getEvent(+route.params['id'])
 
     if (!eventExists)
       this.router.navigate(['/404'])
