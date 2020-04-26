0828fecabe9ff4a769734177d27b4d25717025e1
Bradley Guthrie <guppy96@gmail.com>
2020-04-26 09:13:55 -0500

    attempted to add active link styling but is not visable

--- 7/ng-fundamentals/src/app/nav/navbar.component.ts
+++ 7/ng-fundamentals/src/app/nav/navbar.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
     .nav.navbar-nav {fonr-size: 15px}
     #searchForm {margin-right: 100px;}
     @media (max-width: 1200px) {#searchForm {display: none}}
+    li > a.acive {color: orange;}
   `]
 })
 
