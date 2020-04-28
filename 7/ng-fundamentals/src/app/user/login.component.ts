97e47170a748bf8d20463a419e9eb1591ba1de1d
Bradley Guthrie <guppy96@gmail.com>
2020-04-28 17:16:22 -0500

    added missing template declarations  for username and password in component

--- 7/ng-fundamentals/src/app/user/login.component.ts
+++ 7/ng-fundamentals/src/app/user/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core'
 
 @Component({
+
+
   templateUrl: './login.component.html'
 })
 
 
 export class LoginComponent {
+
+  userName: string
+  password: string
   login(formValues) {
     console.log(formValues)
   }
