45d925e63b784bcdc8337843d6b938a87b24cfcb
Bradley Guthrie <guppy96@gmail.com>
2020-04-27 19:13:41 -0500

    types added for IEvent across main componenets

--- 7/ng-fundamentals/src/app/events/events-list.component.ts
+++ 7/ng-fundamentals/src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared';
 
 
 declare let toastr
@@ -25,7 +26,7 @@ declare let toastr
 })
 
 export class EventsListComponent implements OnInit {
-  events: any
+  events: IEvent[]
 
   constructor(private EventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {
 
