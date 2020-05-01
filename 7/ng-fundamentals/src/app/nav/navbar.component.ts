e676e4e00941cc3580e730414a05b0ffdf77c8ed
Bradley Guthrie <guppy96@gmail.com>
2020-04-30 20:39:07 -0500

    fixed styling on active link

--- 7/ng-fundamentals/src/app/nav/navbar.component.ts
+++ 7/ng-fundamentals/src/app/nav/navbar.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../user/auth.service';
     .nav.navbar-nav {fonr-size: 15px}
     #searchForm {margin-right: 100px;}
     @media (max-width: 1200px) {#searchForm {display: none}}
-    li > a.acive {color: orange;}
+    li > a.active { color: #F97924; }
   `]
 })
 
