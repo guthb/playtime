46f81cdb1d4f33b24261afc44a31d17036ce2550
Bradley Guthrie <guppy96@gmail.com>
2020-05-01 16:40:37 -0500

    wiring up the cancel buttons, auth service was missed

--- 7/ng-fundamentals/src/app/user/profile.component.ts
+++ 7/ng-fundamentals/src/app/user/profile.component.ts
@@ -4,17 +4,7 @@ import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html'
 })
 export class ProfileComponent {
 
@@ -29,14 +19,17 @@ export class ProfileComponent {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
 
-    let firstName = new FormControl()
-    let lastName = new FormControl()
+    let firstName = new FormControl(this.authService.firstName)
+    let lastName = new FormControl(this.authService.lastName)
+
     this.profileForm = new FormGroup({
       firstName: firstName,
       lastName: lastName
     })
+  }
 
-
+  cancel() {
+    this.router.navigate(['events'])
   }
 
 }
