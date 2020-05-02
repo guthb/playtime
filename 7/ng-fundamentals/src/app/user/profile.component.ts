67500df0351c91a044d55617b8c5026bcc75443d
Bradley Guthrie <guppy96@gmail.com>
2020-05-01 20:10:44 -0500

    required was not displaying the correct color

--- 7/ng-fundamentals/src/app/user/profile.component.ts
+++ 7/ng-fundamentals/src/app/user/profile.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service'
 @Component({
   templateUrl: './profile.component.html',
   styles: [`
-    em {float:right; color#E05C65; padding-left:10px}
+    em {float:right; color:#E05C65; padding-left:10px}
     .error input {background-color:#E3C3C5;}
     .error ::-webkit-imput-placeholder {color: #999;}
     .error ::-moz-placeholder {color: #999;}
