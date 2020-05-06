4f4410c410f417660ebcb29a34702bbe90f579d4
Bradley Guthrie <guppy96@gmail.com>
2020-05-05 20:37:47 -0500

    completed the save event method

--- 7/ng-fundamentals/src/app/events/create-event.components.ts
+++ 7/ng-fundamentals/src/app/events/create-event.components.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router';
 
+import { EventService } from '../events/shared/event.service'
+
 
 @Component({
   templateUrl: './create-event.component.html',
@@ -16,12 +18,13 @@ import { Router } from '@angular/router';
 export class CreateEventComponent {
   newEvent
   isDirty: boolean = true;
-  constructor(private router: Router) {
+  constructor(private router: Router, private eventService: EventService) {
 
   }
 
   saveEvent(formValues) {
     console.log(formValues)
+    this.eventService.saveEvent(formValues);
   }
 
 
