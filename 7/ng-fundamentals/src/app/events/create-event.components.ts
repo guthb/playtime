e5cb866e36eecd13716c95567be9810386faecf8
Bradley Guthrie <guppy96@gmail.com>
2020-05-05 20:16:54 -0500

    added object

--- 7/ng-fundamentals/src/app/events/create-event.components.ts
+++ 7/ng-fundamentals/src/app/events/create-event.components.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core'
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: 'create-event.component.html'
+  templateUrl: 'create-event.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left:10px}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-imput-placeholder {color: #999;}
+    .error ::-moz-placeholder {color: #999;}
+    .error :-moz-placeholder {color: #999;}
+    .error :ms-imput-placeholder {color: #999;}
+  `]
 })
 export class CreateEventComponent {
   isDirty: boolean = true;
   constructor(private router: Router) {
 
   }
+
+  saveEvent(formValues) {
+    console.log(formValues)
+  }
+
+
   cancel() {
     this.router.navigate(['/events'])
   }
