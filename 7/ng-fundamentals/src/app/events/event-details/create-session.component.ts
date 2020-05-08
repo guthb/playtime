13f47c13ce34bb55c9a5f81db201ba22890e2969
Bradley Guthrie <guppy96@gmail.com>
2020-05-08 17:23:45 -0500

    none of the validators are working

--- 7/ng-fundamentals/src/app/events/event-details/create-session.component.ts
+++ 7/ng-fundamentals/src/app/events/event-details/create-session.component.ts
@@ -7,7 +7,7 @@ import { ISession, restrictedWords } from '../shared/index'
   templateUrl: './create-session.component.html',
   styles: [`
     em {float:right; color:#E05C65; padding-left:10px}
-    .error input, .error.select, .error textarea {background-color:#E3C3C5;}
+    .error input, .error select, .error textarea {background-color:#E3C3C5;}
     .error ::-webkit-imput-placeholder {color: #999;}
     .error ::-moz-placeholder {color: #999;}
     .error :-moz-placeholder {color: #999;}
