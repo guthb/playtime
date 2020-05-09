cd92807fa9aaa9c2ecab32d0d7d686df922c0ce5
Bradley Guthrie <guppy96@gmail.com>
2020-05-09 18:25:42 -0500

    found error for page load, however validators still not working

--- 7/ng-fundamentals/src/app/events/shared/restricted-words.validator.ts
+++ 7/ng-fundamentals/src/app/events/shared/restricted-words.validator.ts
@@ -9,7 +9,7 @@ export function restrictedWords(words) {
 
     var invalidWords = words
       .map(w => control.value.includes(w) ? w : null)
-      .filer(w => w != null)
+      .filter(w => w != null)
 
 
     return invalidWords && invalidWords.length > 0
