bca0f106ea6f80d3d7c18a63357100cb46c767b6
Bradley Guthrie <guppy96@gmail.com>
2020-05-14 19:32:06 -0500

    setting up required method and new session array to preserve orginal

--- 7/ng-fundamentals/src/app/events/event-details/session-list.component.ts
+++ 7/ng-fundamentals/src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared/index';
 
 
@@ -8,6 +8,26 @@ import { ISession } from '../shared/index';
   templateUrl: './session-list.component.html'
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[]
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
+
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy)
+    }
+  }
+
+
+  filterSessions() {
+    if (filter === 'all') {
+
+    } else {
+
+    }
+  }
+
+
+
 }
