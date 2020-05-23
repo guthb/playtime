1247345c787f04aafaa86adc2daa20bd057af3fe
Bradley Guthrie <guppy96@gmail.com>
2020-05-23 14:01:38 -0500

    cleanup not that we are not using the toaster component here

--- 7/ng-fundamentals/src/app/events/events-list.component.ts
+++ 7/ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
-import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
 import { IEvent } from './shared';
 
@@ -14,7 +13,7 @@ declare let toastr
     <hr>
     <div class="row">
      <div class="col-md-5" *ngFor="let event of events">
-      <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
+      <event-thumbnail [event]="event"></event-thumbnail>
     </div>
     </div>
   </div>
@@ -28,7 +27,7 @@ declare let toastr
 export class EventsListComponent implements OnInit {
   events: IEvent[]
 
-  constructor(private EventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {
+  constructor(private EventService: EventService, private route: ActivatedRoute) {
 
   }
 
@@ -40,10 +39,6 @@ export class EventsListComponent implements OnInit {
 
     this.events = this.route.snapshot.data['events']
 
-  }
-
-  handleThumbnailClick(eventName) {
-    this.toastr.success(eventName)
   }
 
 }
