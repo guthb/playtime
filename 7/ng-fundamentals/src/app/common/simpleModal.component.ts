b468f72aa685e2dc78a75266b7528c103c65a1be
Bradley Guthrie <guppy96@gmail.com>
2020-05-27 16:33:43 -0500

    adding view child

--- 7/ng-fundamentals/src/app/common/simpleModal.component.ts
+++ 7/ng-fundamentals/src/app/common/simpleModal.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
   selector: 'simple-modal',
   template: `
-  <div id="{{elementId}}" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+  <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
       <div class="modal content">
         <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+          <button type="button" class="close"
+            data-dismiss="modal"><span>&times;</span></button>
           <h4 class="modal-title">{{title}}</h4>
         </div>
-        <div class="modal.body">
+        <div class="modal-body" click="closeModal()">
           <ng-content></ng-content>
         </div>
       </div>
@@ -19,10 +20,17 @@ import { Component, Input } from '@angular/core';
   </div>
   `,
   styles: [`
-.modal-body {height: 250px; overflow-y; scroll;}
+.bg-modal-body {height: 250px; overflow-y; scroll;}
 `]
 })
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalcontainer') containerElement: ElementRef
+
+
+  closeModal() {
+    $().modal('hide');
+  }
+
 }
