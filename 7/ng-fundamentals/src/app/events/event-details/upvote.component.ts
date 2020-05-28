40edc20aeb727f057f9111c08659cfe70a042221
Bradley Guthrie <guppy96@gmail.com>
2020-05-28 18:30:25 -0500

    auto complete got me on event emitter

--- 7/ng-fundamentals/src/app/events/event-details/upvote.component.ts
+++ 7/ng-fundamentals/src/app/events/event-details/upvote.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, Output } from '@angular/core';
-import { EventEmitter } from 'protractor';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'upvote',
