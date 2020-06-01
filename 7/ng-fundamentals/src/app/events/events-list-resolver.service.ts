11e63727c795e93d91628da86a217aff5c151044
Bradley Guthrie <guppy96@gmail.com>
2020-06-01 06:06:32 -0500

    removed the call to itself

--- 7/ng-fundamentals/src/app/events/events-list-resolver.service.ts
+++ 7/ng-fundamentals/src/app/events/events-list-resolver.service.ts
@@ -10,7 +10,7 @@ export class EventsListResolver implements Resolve<any> {
 
   }
   resolve() {
-    return this.eventService.getEvents().pipe(map(events => events))
-
+    //return this.eventService.getEvents().pipe(map(events => events))
+    return this.eventService.getEvents()
   }
 }
