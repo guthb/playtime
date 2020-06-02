0e45d6c76e203af0600250c1c40fbd1cf2572982
Bradley Guthrie <guppy96@gmail.com>
2020-06-02 06:17:44 -0500

    subscribed to observable

--- 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
+++ 7/ng-fundamentals/src/app/events/event-details/event-details.component.ts
@@ -20,32 +20,35 @@ export class EventDetailsComponent implements OnInit {
   constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.route.params.forEach((parms: Params) => {
-      this.event = this.eventService.getEvent(+parms['id'])
+    this.route.params.forEach((params: Params) => {
+      this.eventService.getEvent(+params['id']).subscribe((event: IEvent) => {
+        this.event = event;
+        this.addMode = false;
+      })
     })
-
-
-    //this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
-    //this.event = this.eventService.getEvent(1)
-
   }
 
-  addSession() {
-    this.addMode = true
-  }
+  //this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+  //this.event = this.eventService.getEvent(1)
+
+}
+
+addSession() {
+  this.addMode = true
+}
 
 
-  saveNewSession(session: ISession) {
-    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id))
-    session.id = nextId + 1
-    this.event.sessions.push(session)
-    this.eventService.updateEvent(this.event)
-    this.addMode = false
-  }
+saveNewSession(session: ISession) {
+  const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id))
+  session.id = nextId + 1
+  this.event.sessions.push(session)
+  this.eventService.updateEvent(this.event)
+  this.addMode = false
+}
 
-  cancelAddSession() {
-    this.addMode = false
-  }
+cancelAddSession() {
+  this.addMode = false
+}
 
 
 }
