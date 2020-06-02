298ddf4b568343e779963768712dae573b24b29c
Bradley Guthrie <guppy96@gmail.com>
2020-06-02 06:13:37 -0500

    changed to observable

--- 7/ng-fundamentals/src/app/events/shared/event.service.ts
+++ 7/ng-fundamentals/src/app/events/shared/event.service.ts
@@ -19,15 +19,10 @@ export class EventService {
       .pipe(catchError(this.handleError<IEvent[]>('getEvents', [])))
   }
 
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
-    }
-  }
+  getEvent(id: number): Observable<IEvent> {
+    return this.http.get<IEvent>('/api/events' + id)
+      .pipe(catchError(this.handleError<IEvent>('getEvents')))
 
-  getEvent(id: number): IEvent {
-    return EVENTS.find(event => event.id === id)
   }
 
   saveEvent(event) {
@@ -60,6 +55,12 @@ export class EventService {
       emitter.emit(results);
     }, 100);
     return emitter;
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    }
   }
 }
 
