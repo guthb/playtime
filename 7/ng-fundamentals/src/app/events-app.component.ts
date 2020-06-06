ac36397db23a342da89b9ca482cef0d82c8f7e43
Bradley Guthrie <guppy96@gmail.com>
2020-06-06 05:50:45 -0500

    added auth check

--- 7/ng-fundamentals/src/app/events-app.component.ts
+++ 7/ng-fundamentals/src/app/events-app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './user/auth.service';
 
 @Component({
   selector: 'events-app',
-  //template: `
-  //   <h2>Hello World</h2>
-  //   <img src="/assets/basic-shield.png"/>
-  //   `
   template: `
   <nav-bar></nav-bar>
-  <!-- <events-list></events-list> -->
   <router-outlet></router-outlet>
   `
 })
 export class EventsAppComponent {
-  //itle = 'app';
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.auth.checkAuthenticationStatus();
+  }
 }
