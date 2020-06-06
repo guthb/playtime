af3251a3421bfae59dbfbf234b3a2446ea74b7fc
Bradley Guthrie <guppy96@gmail.com>
2020-06-06 05:54:06 -0500

    revert

--- 7/ng-fundamentals/src/app/events-app.component.ts
+++ 7/ng-fundamentals/src/app/events-app.component.ts
@@ -14,6 +14,6 @@ export class EventsAppComponent {
   ngOnInit() {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.auth.checkAuthenticationStatus().subscribe;
+    this.auth.checkAuthenticationStatus();
   }
 }
