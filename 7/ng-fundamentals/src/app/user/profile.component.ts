4c49281ce6288e3572ac2665c0f5269ca6c9a31f
Bradley Guthrie <guppy96@gmail.com>
2020-06-06 16:33:52 -0500

    added observable

--- 7/ng-fundamentals/src/app/user/profile.component.ts
+++ 7/ng-fundamentals/src/app/user/profile.component.ts
@@ -46,8 +46,10 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues) {
     if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-      this.toastr.success('Profile Saved');
-      //this.router.navigate(['events'])
+        .subscribe(() =>) {
+        this.toastr.success('Profile Saved');
+        //this.router.navigate(['events'])
+      }
     }
   }
   validateFirstName() {
