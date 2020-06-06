f25c39a7c3fec55ec3014085b2244406a203eee5
Bradley Guthrie <guppy96@gmail.com>
2020-06-05 19:15:02 -0500

    converted login to http

--- 7/ng-fundamentals/src/app/user/auth.service.ts
+++ 7/ng-fundamentals/src/app/user/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +10,28 @@ import { IUser } from './user.model';
 export class AuthService {
   public currentUser: IUser
 
+  constructor(private http: HttpClient) { }
+
   loginUser(userName: string, password: string) {
+
     //faking the method retun for now
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: 'Frank',
-      lastName: 'Beans'
-    }
+    // this.currentUser = {
+    //   id: 1,
+    //   userName: userName,
+    //   firstName: 'Frank',
+    //   lastName: 'Beans'
+    // }
+
+    let loginInfo = { username: userName, password: password };
+    let options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
+    return this.http.post('/api/login', loginInfo, options).pipe(tap(data => {
+      this.currentUser = <IUser>data['user'];
+    }))
+      .pipe(catchError(err => {
+        return of(false)
+      }))
+
   }
 
   isAuthenticated() {
@@ -27,5 +44,4 @@ export class AuthService {
 
   }
 
-  constructor() { }
 }
