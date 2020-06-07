6b92d916aa40b37ffe0f1da0b6d6f73359cffdae
Bradley Guthrie <guppy96@gmail.com>
2020-06-07 07:48:56 -0500

    added logout method

--- 7/ng-fundamentals/src/app/user/auth.service.ts
+++ 7/ng-fundamentals/src/app/user/auth.service.ts
@@ -67,4 +67,9 @@ export class AuthService {
 
   }
 
+  logout() {
+    let options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post('/api/logout', {}, options);
+  }
+
 }
