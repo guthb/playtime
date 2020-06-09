dcbbbecd8e84a4a67353c625e9a8bef3f92f3691
Bradley Guthrie <guppy96@gmail.com>
2020-06-09 06:42:03 -0500

    test writen as instructed, does not pass

--- 7/ng-fundamentals/src/app/events/event-details/voter.service.spec.ts
+++ 7/ng-fundamentals/src/app/events/event-details/voter.service.spec.ts
@@ -1,23 +1,29 @@
 import { VoterService } from './voter.service'
 import { ISession } from '../shared/event.model'
-import { Observable } from 'rxjs'
+import { Observable, of, from } from 'rxjs'
 
 describe('VoterService', () => {
-  let voterService: VoterService,
-    mockHttp;
+  let voterService: VoterService, mockHttp;
 
 
   beforeEach(() => {
-    mockHttp = jasmine.createSpyObj('mockHttp', [])
+    mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post']
+    )
     voterService = new VoterService(mockHttp);
   });
 
   describe('deleteVoter', () => {
 
     it('should remove the voter from the list of voters', () => {
-      var session = { id: 6, voters: ["ben", "sara"] };
+      var session = { id: 6, voters: ['joe', 'john'] };
+
+      mockHttp.delete.and.returnedValue(of(false));
 
       voterService.deleteVoter(3, <ISession>session, "joe");
+
+      expect(session.voters.length).toBe(1);
+
+      expect(session.voters[0]).toBe("john");
     })
   })
 
