c458a40e9c280347ea339bf712bfde86efb5b349
Bradley Guthrie <guppy96@gmail.com>
2020-06-10 06:01:47 -0500

    added second test

--- 7/ng-fundamentals/src/app/events/event-details/voter.service.spec.ts
+++ 7/ng-fundamentals/src/app/events/event-details/voter.service.spec.ts
@@ -25,6 +25,19 @@ describe('VoterService', () => {
 
       expect(session.voters[0]).toBe("john");
     })
+
+    it('should call http.delete with the right URL', () => {
+      var session = { id: 6, voters: ['joe', 'john'] };
+
+      mockHttp.delete.and.returnedValue(of(false));
+
+      voterService.deleteVoter(3, <ISession>session, "joe");
+
+      expect(mockHttp.delete).toHaveBeenCalled(`/api/events/3/sessions/3/voters/joe`);
+
+      //expect(session.voters[0]).toBe("john");
+    })
+
   })
 
 })
