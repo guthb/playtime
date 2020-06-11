0a320cbeb1e340658dc6150837f2c64d7a8ca550
Bradley Guthrie <guppy96@gmail.com>
2020-06-11 06:57:09 -0500

    updated small format tweak, still not passing

--- 7/ng-fundamentals/src/app/events/event-details/session-list.component.spec.ts
+++ 7/ng-fundamentals/src/app/events/event-details/session-list.component.spec.ts
@@ -10,7 +10,8 @@ describe('SessionListComponent', () => {
   })
 
   describe('ngOnChanges', () => {
-    it('should fileter the seddions correctly', () => {
+
+    it('should filter the sessions correctly', () => {
       component.sessions = <ISession[]>[{ name: 'session 1', level: 'intermediate' },
       { name: 'session 2', level: 'intermediate' },
       { name: 'session 3', level: 'beginner' }]
@@ -20,8 +21,35 @@ describe('SessionListComponent', () => {
 
       component.ngOnChanges();
 
+      expect(component.visibleSessions.length).toBe(2);
+
+    })
+
+    it('should filter the sessions correctly', () => {
+      component.sessions = <ISession[]>[{ name: 'session 1', level: 'intermediate' },
+      { name: 'session 2', level: 'intermediate' },
+      { name: 'session 3', level: 'beginner' }]
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
 
       expect(component.visibleSessions.length).toBe(2);
+
+    })
+
+    it('should sort the sessions correctly', () => {
+      component.sessions = <ISession[]>[{ name: 'session 1', level: 'intermediate' },
+      { name: 'session 3', level: 'intermediate' },
+      { name: 'session 2', level: 'beginner' }]
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3;
+
+      component.ngOnChanges();
+
+      expect(component.visibleSessions[2].name).toBe('session 3');
 
     })
   })
