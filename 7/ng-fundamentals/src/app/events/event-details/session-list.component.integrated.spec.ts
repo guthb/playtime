cc4d7fc454351741892b487bb7cdaa325dc1389b
Bradley Guthrie <guppy96@gmail.com>
2020-06-12 06:34:22 -0500

    ready to write tests

--- 7/ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
+++ 7/ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing'
-import { DebugElement } from '@angular/core'
+import { DebugElement, Component } from '@angular/core'
 import { By } from '@angular/platform-browser'
 
 import { SessionListComponent } from './session-list.component'
@@ -7,3 +7,34 @@ import { AuthService } from '../../user/auth.service'
 import { VoterService } from './voter.service'
 import { ISession } from '../shared/event.model'
 
+describe('SessionListComponent', () => {
+  let fixture: ComponentFixture<SessionListComponent>,
+    component: SessionListComponent,
+    element: HTMLElement,
+    debugEl: DebugElement
+
+  beforeEach(async(() => {
+    let mockAuthService = {}
+    let mockVoterService = {}
+
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [
+        SessionListComponent
+      ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: VoterService, useValue: mockVoterService }
+      ],
+      schemas: []
+    })
+  }))
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement;
+  })
+
+
+})
