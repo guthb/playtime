95e9207e1001e4d8e2b24cbac565a3f5c922d72d
Bradley Guthrie <guppy96@gmail.com>
2020-06-13 06:37:05 -0500

    new test

--- 7/ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
+++ 7/ng-fundamentals/src/app/events/event-details/session-list.component.integrated.spec.ts
@@ -36,5 +36,18 @@ describe('SessionListComponent', () => {
     element = fixture.nativeElement;
   })
 
+  describe('initial display', () => {
+    it('should have the correct session title', () => {
+      component.sessions = [{
+        id: 3, name: 'Session 1', presenter: 'Joe',
+        duration: 1, level: 'beginner', abstract: 'abstract', voters: ['john', 'bob']
+      }];
+      component.filterBy = 'all';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+    })
+  })
 
 })
